ca9c25699e70f9840ac839c70d0b80b9fb91ada3
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-11-30 01:36:13 +0330

    add add to card action

--- src/components/home/stickers-container.component.jsx
+++ src/components/home/stickers-container.component.jsx
@@ -1,21 +1,44 @@
+import { useEffect } from "react";
 import { Col, Card, Row, Button } from "react-bootstrap";
 import { useCategoryState } from "../../context/category/provider";
-import { FaShoppingBasket } from 'react-icons/fa';
+import { FaShoppingBasket } from "react-icons/fa";
 
 import styles from "./home.module.scss";
+import { useCardDispatch, useCardState } from "../../context/card/provider";
+import { addToCartAction } from "../../context/action/cardAction";
 const StickerContainer = () => {
   const stickers = useCategoryState();
+  const dispatch = useCardDispatch();
+  const state = useCardState();
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
   console.log(stickers);
+  console.log("render","color:'red")
   return (
     <>
-      <h2 className="text-center mt-5" style={{color:'#32325d'}}>برنامه نویسی</h2>
+      <h2 className="text-center mt-5" style={{ color: "#32325d" }}>
+        برنامه نویسی
+      </h2>
       <Row>
         {stickers
           ? stickers.map((item, index) => (
               <Col className={styles.card_collection_container} lg={4} md={6}>
                 <Card>
-                  <Card.Img className={styles.card_collection_image} width={200} height={200} src={item.image} />
-                  <Button className={styles.add_to_card_button}><FaShoppingBasket/></Button>
+                  <Card.Img
+                    className={styles.card_collection_image}
+                    width={200}
+                    height={200}
+                    src={item.image}
+                  />
+                  <Button
+                    className={styles.add_to_card_button}
+                    onClick={()=>dispatch(
+                      addToCartAction(item)
+                    )}
+                  >
+                    <FaShoppingBasket />
+                  </Button>
                   <Card.Body>
                     <Card.Title className="text-center">
                       {item.title}
