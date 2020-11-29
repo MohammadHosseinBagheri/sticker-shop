e7487b3ed26fda799e1f4e57f0a224bb3abe304e
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-11-30 01:05:51 +0330

    local sticker data

---
+++ src/pages/api/data/index.js
@@ -0,0 +1,141 @@
+const stickers = [
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6378/78fda137af9994844d4e8e36e70df03b.png?w=300&h=300&q=90",
+    title: "your opinions belong here",
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6377/4fded1464736e77865df232cbcb4cd19.png?w=300&h=300&q=90",
+    title: "yolo",
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6377/4fded1464736e77865df232cbcb4cd19.png?w=300&h=300&q=90",
+    title: "works on my machine border",
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6376/d85176e5927acd6f5840fe8b7cf09592.png?w=300&h=300&q=90",
+    title: "windows cat",
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6375/505a0826dc0756de8eb7bd4c4e0c34cc.png?w=300&h=300&q=90",
+    title: "will code for pizza",
+
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6374/e8dfc2346b36b3d4ec54973c2fa66459.png?w=300&h=300&q=90",
+    title: "will code for beer",
+
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6373/2205f3bad57847fc78e08c9ed0208bf4.png?w=300&h=300&q=90",
+    title: "wifi connection",
+
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6372/e1fb5f6e14b64292baafc247fb3743cd.png?w=300&h=300&q=90",
+    title: "when i see my code without errors",
+
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6371/39e7794a900a9679e7bf650187afb52d.png?w=300&h=300&q=90",
+    title: "vue js emblem",
+
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6294/2b68e08fecccb616689cf23b42041da1.png?w=300&h=300&q=90",
+    title: "unicode",
+
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6293/8ab3b19e134f01fbaf94b8e15f3df090.png?w=300&h=300&q=90",
+    title: "unexpected error JSSTIC15610",
+
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6292/f2ca0dc2c2a81319a8e478c27e08b99e.png?w=300&h=300&q=90",
+    title: "ui ux b",
+
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6291/6c7694d84718c8599cc568380985bded.png?w=300&h=300&q=90",
+    title: "true that",
+
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6290/06848439ead01cad052baa3cfa0f72bd.png?w=300&h=300&q=90",
+    title: "trespassers will be put to work b",
+
+    price: "2550  تومان",
+  },
+  {
+    image:
+      "https://geektori.ir/uploads/image/rootimage/6289/f718209daa1bc2bf1a18907e4f35e290.png?w=300&h=300&q=90",
+    title: "torrents",
+
+    price: "2550  تومان",
+  },
+];
+
+export default ((req, res) => {
+  res.json(stickers)
+});
+
+// "https://geektori.ir/uploads/image/rootimage/7216/6f4922f45568161a8cdf4ad2299f6d23.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7215/70efdf2ec9b086079795c442636b55fb.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7214/c74d97b01eae257e44aa9d5bade97baf.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7213/9bf31c7ff062936a96d3c8bd1f8f2ff3.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7212/aab3238922bcc25a6f606eb525ffdc56.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7211/c51ce410c124a10e0db5e4b97fc2af39.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7209/c20ad4d76fe97759aa27a0c99bff6710.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7208/6512bd43d9caa6e02c990b0a82652dca.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7207/d3d9446802a44259755d38e6d163e820.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7206/0a8005f5594bd67041f88c6196192646.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7205/fad6f4e614a212e80c67249a666d2b09.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7204/d72d187df41e10ea7d9fcdc7f5909205.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7203/faeac4e1eef307c2ab7b0a3821e6c667.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7201/751d31dd6b56b26b29dac2c0e1839e34.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7200/7d0665438e81d8eceb98c1e31fca80c1.jpg?w=300&h=300&q=90",
+// "https://geektori.ir/uploads/image/rootimage/7199/e45ee7ce7e88149af8dd32b27f9512ce.jpg?w=300&h=300&q=90",
+
+// "استیکر among us shhhhh",
+// "استیکر among us Wasnt me",
+// "استیکر Among us Angel",
+// "استیکر among us Little Babay",
+// "استیکر Among Us Vote",
+// "استیکر among us Corpse",
+// "استیکر Discord",
+// "استیکر among us i dont trust",
+// "استیکر Among us Victory",
+// "استیکر Among us 2",
+// "استیکر among us dream version",
+// "استیکر Among Us - Ejected",
+// "استیکر Among Us Crewmate - Halloween Edition",
+// "استیکر Among Us",
+// "استیکر Among Us World",
+// "استیکر Among Us pink crew mate impostor",
