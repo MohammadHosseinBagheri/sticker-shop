d2a64eeb861418c6a9ecdd4c4b2c8b0e81ed117d
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-11-30 01:36:38 +0330

    add  add to card reducer

--- src/context/reducer/cardReducer.js
+++ src/context/reducer/cardReducer.js
@@ -1,6 +1,13 @@
+import * as types from "../../constant/types";
 
-const reducer = (state , action) => {
+const reducer = (state, action) => {
   switch (action.type) {
+    case types.ADD_TO_CARD:
+      console.log(action.payload)
+      return {
+        ...state,
+        item: action.payload,
+      };
     default:
       return state;
   }
