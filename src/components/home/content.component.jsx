08a9ccecd052ac18b8138210aba00c7c8ea6a913
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-11-30 12:16:22 +0330

    edit pagination

--- src/components/home/content.component.jsx
+++ src/components/home/content.component.jsx
@@ -11,18 +11,30 @@ import SwiperCore, {
 import { Swiper, SwiperSlide } from "swiper/react";
 import MySwiper from "./swiper.component";
 import StickerContainer from "./stickers-container.component";
-
+import styles from "./home.module.scss";
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, EffectFade]);
 const HomeContent = () => {
   const category = useCategoryState();
-  console.log('content render','color:red;')
+  console.log("content render", "color:red;");
   return (
     <>
       <Row>
         <SwiperTitle />
         <Swiper
-          spaceBetween={30}
-          slidesPerView={4}
+          paginationStyle={{ bottom: 15 }}
+          pagination={{ clickable: true }}
+          className={styles.swiper_container}
+          breakpoints={{
+            640: {
+              width: 640,
+              slidesPerView: 1,
+            },
+            768: {
+              width: 768,
+              slidesPerView: 2,
+            },
+          }}
+          dynamicBullets={true}
           parallax
           autoplay={true}
           speed={500}
@@ -38,7 +50,7 @@ const HomeContent = () => {
             : null}
         </Swiper>
       </Row>
-        <StickerContainer />
+      <StickerContainer />
     </>
   );
 };
