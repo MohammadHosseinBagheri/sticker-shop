75b9fa8c10c446343ac85fede2b29ea02ef9d363
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-12-06 14:08:44 +0330

    add usePrice

--- src/context/card/provider.js
+++ src/context/card/provider.js
@@ -34,4 +34,48 @@ const useQuantity = () => {
   return quantity;
 };
 
-export { useCardDispatch, useCardState, useQuantity };
+const usePrice = () => {
+  var persianNumbers = [
+    /۰/g,
+    /۱/g,
+    /۲/g,
+    /۳/g,
+    /۴/g,
+    /۵/g,
+    /۶/g,
+    /۷/g,
+    /۸/g,
+    /۹/g,
+  ];
+  var arabicNumbers = [
+    /٠/g,
+    /١/g,
+    /٢/g,
+    /٣/g,
+    /٤/g,
+    /٥/g,
+    /٦/g,
+    /٧/g,
+    /٨/g,
+    /٩/g,
+  ];
+
+  const fixNumbers = function (str) {
+    if (typeof str === "string") {
+      for (var i = 0; i < 10; i++) {
+        str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+      }
+    }
+    return str;
+  };
+  const state = useCardState();
+  const price = state?.reduce((price, item) => {
+    const slice1 = item.price.split("٫")[0];
+    const slice2 = item.price.split("٫")[1];
+    const mergeNumber = slice1 + slice2;
+    return price + item.quantity * fixNumbers(mergeNumber.split(" ")[0]);
+  }, 0);
+  return price;
+};
+
+export { useCardDispatch, useCardState, useQuantity, usePrice };
