395ec08fc8fe24ed7508b6bf444607977803ea99
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-12-06 13:30:49 +0330

    edit payloads

--- src/context/action/cardAction.js
+++ src/context/action/cardAction.js
@@ -3,20 +3,21 @@ import * as types from "../../constant/types";
 const addToCartAction = (payload) => {
   return {
     type: types.ADD_TO_CARD,
-    payload: payload,
+    payload,
   };
 };
 
 const removeFromCardAction = (payload) => {
   return {
     type: types.REMOVE_FROM_CARD,
-    payload: payload,
+    payload,
   };
 };
 
 const deleteCardAction = (payload) => {
   return {
     type: types.DELETE_CARD,
+    payload,
   };
 };
 
