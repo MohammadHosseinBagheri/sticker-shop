e81a651d2b651309b287fe977a2cbf752629e6cd
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-12-06 13:31:53 +0330

    add addtocard remove and delete

--- src/context/reducer/cardReducer.js
+++ src/context/reducer/cardReducer.js
@@ -1,13 +1,14 @@
 import * as types from "../../constant/types";
+import { add, remove, deleteItem } from "../../utils/addToCart";
 
 const reducer = (state, action) => {
   switch (action.type) {
     case types.ADD_TO_CARD:
-      console.log(action.payload)
-      return {
-        ...state,
-        item: action.payload,
-      };
+      return add(state, action.payload);
+    case types.REMOVE_FROM_CARD:
+      return remove(state, action.payload);
+    case types.DELETE_CARD:
+      return deleteItem(state, action.payload);
     default:
       return state;
   }
