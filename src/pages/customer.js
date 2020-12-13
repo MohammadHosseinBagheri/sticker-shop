13f02b7d5dbb8c165235e477d62e5aea7a545ffc
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-12-13 11:17:00 +0330

    add customer

---
+++ src/pages/customer.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import Content from "../components/customer-content/content.component";
+
+const Customer = () => {
+  return (
+    <Container>
+      <Content />
+    </Container>
+  );
+};
+
+export default Customer;
