d8435545d3666b7fe7353b148fb9870418310ee1
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-12-16 17:14:39 +0330

    fix sth

--- src/pages/customer.js
+++ src/pages/customer.js
@@ -3,7 +3,7 @@ import Content from "../components/customer-content/content.component";
 
 const Customer = () => {
   return (
-    <Container>
+    <Container >
       <Content />
     </Container>
   );
