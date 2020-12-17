1d0057d23cc874661aeac354daedac2ce9027451
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-12-17 09:11:51 +0330

    next config

--- next.config.js
+++ next.config.js
@@ -8,21 +8,5 @@ module.exports = {
   },
   pagesDir: "./src/pages",
   target: "serverless",
-  async headers() {
-    return [
-      {
-        source: '/about',
-        headers: [
-          {
-            key: 'x-custom-header',
-            value: 'my custom header value',
-          },
-          {
-            key: 'x-another-custom-header',
-            value: 'my other custom header value',
-          },
-        ],
-      },
-    ]
-  },
+  
 };
