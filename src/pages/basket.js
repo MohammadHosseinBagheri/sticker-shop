9e3fdba3a37b82a1449cc6232f276382cb3d0f3e
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-12-21 22:50:08 +0330

    comment useEffect

--- src/pages/basket.js
+++ src/pages/basket.js
@@ -11,40 +11,41 @@ import Customer from "./customer";
 import { FaArrowRight } from "react-icons/fa";
 import { useEffect } from "react";
 import Head from "next/head";
+import WithBasket from "../hoc/with-basket.component";
 
 const Basket = () => {
   const cardState = useCardState();
   const dispatch = useCardDispatch();
   const price = usePrice();
   const router = useRouter();
-  useEffect(() => {
-    if (cardState.length === 0) {
-      router.replace("/");
-    }
-  }, [cardState]);
+  // useEffect(() => {
+  //   if (cardState.length === 0) {
+  //     router.replace("/");
+  //   }
+  // }, [cardState]);
 
   return (
     <>
-    <Head >
-      <title>سبد خرید</title>
-    </Head>
-    <Container>
-      <BasketTable />
-      <div className="d-flex flex-row justify-content-between">
-        <h2 style={{ direction: "rtl" }}>قیمت کل:{price}$</h2>
-        <Link href="/customer">
-          <a
-            style={{ background: "#00E676" }}
-            className="align-self-center p-2 rounded-sm "
-          >
-            ادامه مراحل و پرداخت
-            <FaArrowRight />
-          </a>
-        </Link>
-      </div>
-    </Container>
+      <Head>
+        <title>سبد خرید</title>
+      </Head>
+      <Container>
+        <BasketTable />
+        <div className="d-flex flex-row justify-content-between">
+          <h2 style={{ direction: "rtl" }}>قیمت کل:{price}$</h2>
+          <Link href="/customer">
+            <a
+              style={{ background: "#00E676" }}
+              className="align-self-center p-2 rounded-sm "
+            >
+              ادامه مراحل و پرداخت
+              <FaArrowRight />
+            </a>
+          </Link>
+        </div>
+      </Container>
     </>
   );
 };
 
-export default Basket;
+export default WithBasket(Basket);
