6c55094b3a795bab559de17b880a91eb614dd6b7
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-12-22 00:16:15 +0330

    add downside of basketside

--- src/components/basket-side/basket-side.component.jsx
+++ src/components/basket-side/basket-side.component.jsx
@@ -1,12 +1,13 @@
-import { useCardState } from "../../context/card/provider";
+import { useCardState, usePrice } from "../../context/card/provider";
 import { Col } from "react-bootstrap";
 import styles from "./basket-side.module.scss";
 import BasketSideItem from "./basket-side-item.component";
 const BasketSide = () => {
   const cardItem = useCardState();
+  const price = usePrice();
   return (
     <Col
-      className={`d-none d-md-flex justify-content-end flex-row ${styles.basket_side_container} `}
+      className={`d-none d-md-flex justify-content-end flex-column ${styles.basket_side_container} `}
       lg={3}
       md={4}
     >
@@ -15,7 +16,14 @@ const BasketSide = () => {
           <BasketSideItem key={index} item={item} />
         ))}
       </Col>
-      <hr />
+      <div style={{direction:'rtl'}} className="d-flex flex-column mt-2 justify-content-end">
+        <h5 style={{textAlign:'right'}}>کد تخفیف</h5>
+        <strong style={{ color: "#6B7C93",textAlign:'right' }}>
+          مبلغ کل سبد خرید : {price} تومان{" "}
+        </strong>
+        <p style={{ color: "#6B7C93",textAlign:'right' }} className="mt-2" >هزینه ارسال : 0</p>
+        <p style={{ color: "#6B7C93",textAlign:'right' }} >هزینه قابل پرداخت : 0</p>
+      </div>
     </Col>
   );
 };
