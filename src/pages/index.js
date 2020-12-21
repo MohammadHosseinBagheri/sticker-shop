04c80a76cca411c2a471681aabc1f9794859dc9f
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-12-21 12:04:03 +0330

    add head

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,20 @@ import HomeContent from "../components/home/content.component";
 import { useCategoryAction } from "../context/category/provider";
 import { useGetAllCategory } from "../hooks";
 import stickers from "./api/data";
+import Head from "next/head";
 
 const Home = (props) => {
   const setData = useCategoryAction();
   setData(stickers);
   return (
-    <Container>
-      <HomeContent />
-    </Container>
+    <>
+      <Head>
+        <title>استیکر ملیا</title>
+      </Head>
+      <Container>
+        <HomeContent />
+      </Container>
+    </>
   );
 };
 
