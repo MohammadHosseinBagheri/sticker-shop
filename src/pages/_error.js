f67d0a12358ff1267a7bb5753145708b768fd4ee
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-12-24 22:56:43 +0330

    add header

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,16 +1,22 @@
 import Link from "next/link";
+import Head from "next/head";
 const Error = ({ statusCode }) => {
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center">
-      <h4>
-        {statusCode
-          ? `دست و پای بچه ها گیر کرده به سیم ${statusCode}`
-          : "خودت باعث ایجاد ارور شدی "}
-      </h4>
-      <Link href="/">
-        <a className="btn btn-danger mt-5 mb-5"> منو ببر به خونه </a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>به ارور برخورد کردی</title>
+      </Head>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h4>
+          {statusCode
+            ? `دست و پای بچه ها گیر کرده به سیم ${statusCode}`
+            : "خودت باعث ایجاد ارور شدی "}
+        </h4>
+        <Link href="/">
+          <a className="btn btn-danger mt-5 mb-5"> منو ببر به خونه </a>
+        </Link>
+      </div>
+    </>
   );
 };
 
