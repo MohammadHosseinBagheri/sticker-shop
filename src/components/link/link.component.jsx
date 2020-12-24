7040e953c744ee90190669422aa59da2b24b8d21
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-12-24 22:31:45 +0330

    edit link component

--- src/components/link/link.component.jsx
+++ src/components/link/link.component.jsx
@@ -4,8 +4,8 @@ import styles from "./header.module.scss";
 const MyLink = (props) => {
   const { href, title } = props;
   return (
-    <Link className={styles.headerlink} href={href}>
-      <a>{title}</a>
+    <Link  href={href}>
+      <a className={`d-none d-md-flex ${styles.headerlink}`} >{title}</a>
     </Link>
   );
 };
